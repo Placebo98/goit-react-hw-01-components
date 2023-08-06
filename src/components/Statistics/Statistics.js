50d8c01530205fc4d5a89a0f2cce0a1b940230a2
Placebo98 <oleg.tsaryk98@gmail.com>
2023-08-06 03:30:53 +0300

    Start Project, add data files. render Profiles and Statistics

---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,15 @@
+export function Statistics({ title, stats }) {
+  return (
+    <section className="statistics">
+      <h2 className="title">Upload stats</h2>
+      <ul className="stat-list">
+        {stats.map(item => (
+          <li className="item" key={item.id}>
+            <span className="label">{item.label}</span>
+            <span className="percentage">{item.percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
